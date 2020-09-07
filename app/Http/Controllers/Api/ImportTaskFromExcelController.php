cfdf96f5f74532a538957adfdcac068e00a81d2b
elyabe <elyabe.as@gmail.com>
2020-09-07 12:11:45 -0300

    Handle Upload file import

--- app/Http/Controllers/Api/ImportTaskFromExcelController.php
+++ app/Http/Controllers/Api/ImportTaskFromExcelController.php
@@ -8,6 +8,7 @@ use App\Task;
 use DateTime;
 use DateTimeZone;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use LaravelDoctrine\ORM\Facades\EntityManager;
 use Rap2hpoutre\FastExcel\FastExcel;
@@ -16,17 +17,41 @@ class ImportTaskFromExcelController extends Controller
 {
     public function import(Request $request)
     {
+        $requestValidator = Validator::make($request->all(), [
+            'file' => 'required|mimes:xlsx',
+        ]);
+
+        if ($requestValidator->fails()) {
+            return response()->json([
+                'error' => $requestValidator->errors(),
+            ], 400);
+        }
+
+        $file = $request->file('file');
+        $prefixUniq = uniqid(date('HisYmd'));
+        $name = $file->getFilename();
+        $extension = $file->extension();
+        $filename = "{$prefixUniq}-{$name}.{$extension}";
+
+        $upload = $file->storeAs('tmp', $filename);
+
+        if (!$upload) {
+            throw new ApiException("Error on upload tasks file", 400);
+        }
+
+        $filePath = storage_path("app/tmp/{$filename}");
+
         $user = $request->user('api');
         $fastExcel = new FastExcel();
-        $taskRows = $fastExcel->import(storage_path('app') . '/tasks.xlsx', function ($row) {
-            return $row;
-        });
+        $taskRows = $fastExcel->import($filePath);
 
-        foreach ($taskRows as $taskRow) {
+        foreach ($taskRows as $key => $taskRow) {
             $validator = $this->taskValidator($taskRow);
 
             if ($validator->fails()) {
-                throw new ApiException("Error Processing File", 400);
+                $exceptionLine = $key + 1;
+                Storage::disk('local')->delete("tmp/{$filename}");
+                throw new ApiException("Error Processing File on row {$exceptionLine}", 400);
             }
 
             $task = new Task;
@@ -41,16 +66,19 @@ class ImportTaskFromExcelController extends Controller
 
         EntityManager::flush();
 
+        Storage::disk('local')->delete("tmp/{$filename}");
+
         return response()->json([
             'message' => 'Tasks imported from file :D',
+            'quantity' =>  $taskRows->count(),
         ]);
     }
 
     private function taskValidator($data)
     {
         return Validator::make($data, [
-            'title' => 'required',
-            'date' => 'required|date'
+            'title' => 'required|string',
+            'date' => 'required|date|date_format:Y-m-d H:i:s',
         ]);
     }
 }
