64491b73fc4ce6efb2a928a1819e00081a980051
elyabe <elyabe.as@gmail.com>
2020-09-13 10:35:44 -0300

    Add start.sh

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
+
+php /var/www/html/artisan config:cache
+
+
+if [ "$role" = "app" ]; then
+
+    exec apache2-foreground
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan queue:work --memory=3800 --queue=bigjob --timeout=9600 --tries=1
+
+elif [ "$role" = "scheduler" ]; then
+
+    while [ true ]
+    do
+      php /var/www/html/artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
